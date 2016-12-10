7be6c2fea10292d4ac3b214a095a6269a2ac91d7
Stefan Koolen <nast3zz@gmail.com>
2016-12-10 03:58:22 +0100

    Added more models

--- FitYourFood/FitYourFood/FitYourFood/Model/Recipe.cs
+++ FitYourFood/FitYourFood/FitYourFood/Model/Recipe.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FitYourFood.Model
+namespace FitYourFood.Model.Recipe
 {
     public class Step
     {
---
+++ FitYourFood/FitYourFood/FitYourFood/Model/RecipeList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitYourFood.Model.RecipeList
+{
+    public class RecipeInformation
+    {
+        public string recipeTitle { get; set; }
+        public string recipeId { get; set; }
+        public string lastUpdatedTime { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string serverTime { get; set; }
+        public List<RecipeInformation> recipeInformation { get; set; }
+        public string message { get; set; }
+    }
+
+}
